19c84a7ee5de9afc935eae97cc88110749300de0
James Swift <sbcjames86@yahoo.com>
2020-09-08 23:05:38 -0700

    found syntax error and correct, stupid }

--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@
 $().ready(() => {
   const animalsArray = [];
 
-  $.ajax('../data/page-1.json', {method: 'get', dataType: 'JSON'})
-    .then(hornedAnimals => {
-      console.log(hornedAnimals);
+  $.ajax('data/page-1.json', {method: 'get', dataType: 'JSON'})
+    .then((hornedAnimals) => {
       hornedAnimals.forEach((horned) => {
         new Animal(horned).render();
-      })
+      });
       populateDropdown();
     // $('select').on('change', filterHorns);
-    })
+    }
+    );
   function Animal(object) {
     this.title = object.title;
     this.description = object.description;
@@ -27,37 +27,36 @@ $().ready(() => {
 
     $newSection.find('h2').text(this.title);
     $newSection.find('p').text(this.description);
-    $newSection.find('img').attr('src',this.image_url);
+    $newSection.find('img').attr('src', this.image_url);
 
     $('main').append($newSection);
 
-  }
+  };
   const populateDropdown = () => {
     const keywordArray = [];
-    // const $dropDown = $('select');
     animalsArray.forEach((animal) => {
       if (keywordArray.includes(animal.keyword) === false) {
         keywordArray.push(animal.keyword);
       }
+    });
+    keywordArray.forEach((keyword) => {
+      const $option = $(`<option></option>`);
+      $option.text(keyword);
+      $option.attr('value', keyword);
+      $('select').append($option);
+    });
+    console.log(keywordArray)
+  };
 
-      keywordArray.forEach((keyword) => {
-        const $option = $(`<option></option>`);
-        $option.text(keyword);
-        $option.attr('value', keyword);
-        $('select').append($option);
-      });
-      console.log(keywordArray)
-    })
-
-    $('#menu').on('change', eventHandler);
-    function eventHandler(event) {
-      $('section').hide();
-      animalsArray.forEach((object) => {
-        if (event.target.value === object.keyword) {
+  $('#menu').on('change', eventHandler);
+  function eventHandler(event) {
+    $('section').hide();
+    animalsArray.forEach((object) => {
+      if (event.target.value === object.keyword) {
         // console.log(object);
-          $(`section[class = ${object.keyword}]`).show();
-        }
-      });
-    }
+        $(`section[class = ${object.keyword}]`).show();
+      }
+    });
   }
-})
+});
+
